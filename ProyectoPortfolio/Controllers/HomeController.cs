b5cc0fa8ab9a95788625646466c6fb134793b7e8
nicocastx <nicocastx@gmail.com>
2023-05-06 16:34:10 -0300

    mejora, uso de services

--- ProyectoPortfolio/Controllers/HomeController.cs
+++ ProyectoPortfolio/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProyectoPortfolio.Models;
+using ProyectoPortfolio.Services;
 using System.Diagnostics;
 
 namespace ProyectoPortfolio.Controllers
@@ -26,48 +27,13 @@ namespace ProyectoPortfolio.Controllers
             // HomeController y el nombre de la funcion Index
             //en view recibe de parametros el nombre de la vista y algun modelo a representar
             //return View("Index", "Roberto");
-            var proyectos = ObtenerProyectos().Take(2).ToList();
+            var repositorioProyectos = new RepositorioProyectos();
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(2).ToList();
             var model = new HomeIndexViewModel()
             {
                 Proyectos = proyectos
             };
             return View("Index", model);
-        }
-
-        private List<ProyectDTO> ObtenerProyectos()
-        {
-            return new List<ProyectDTO>
-            {
-                new ProyectDTO
-                {
-                    Titulo =  "Game-Place",
-                    Descripcion = "Este proyecto se trata de una tienda online de videojuegos donde se puede comprar a traves de un stock una cantidad de juegos especificos de una base de datos subida en Firebase, ademas del uso de contexto para carritos y routing",
-                    ImagenURL = "/images/proyectos/proyectoGamePlaceReact.png",
-                    Link ="https://github.com/nicocastx/Game-Place",
-                },
-                new ProyectDTO
-                {
-                    Titulo =  "Instrumenty",
-                    Descripcion = "Proyecto orientado al backend en donde desarrollo una aplicacion orientada al back-end",
-                    ImagenURL = "/images/proyectos/proyectoInstrumentyBackend.png",
-                    Link ="https://github.com/nicocastx/Game-Place",
-                },
-                new ProyectDTO
-                {
-                    Titulo =  "Criptowin",
-                    Descripcion = "Criptowin es un proyecto realizado para demostrar mis conocimiento" +
-                    "adquiridos durante el cursado de Desarrollo Web en Coderhouse",
-                    ImagenURL = "/images/proyectos/proyectoCriptoWinDW.png",
-                    Link = "https://criptowin-4ctdw1k5j-nicocastx.vercel.app/"
-                },
-                new ProyectDTO
-                {
-                    Titulo =  "SteamSim",
-                    Descripcion = "SteamSim es mi proyecto desarrollado para el curso de Javascript en la Academia de Coderhouse ",
-                    ImagenURL = "/images/proyectos/proyectoSteamSimJS.png",
-                    Link ="https://steam-sim-coder-a0cy0c1h6-nicocastx.vercel.app/",
-                },
-            };
         }
 
         public IActionResult Privacy()
---
+++ ProyectoPortfolio/Services/RepositorioProyectos.cs
@@ -0,0 +1,43 @@
+﻿using ProyectoPortfolio.Models;
+
+namespace ProyectoPortfolio.Services
+{
+    public class RepositorioProyectos
+    {
+        public List<ProyectDTO> ObtenerProyectos()
+        {
+            return new List<ProyectDTO>
+            {
+                new ProyectDTO
+                {
+                    Titulo =  "Game-Place",
+                    Descripcion = "Este proyecto se trata de una tienda online de videojuegos donde se puede comprar a traves de un stock una cantidad de juegos especificos de una base de datos subida en Firebase, ademas del uso de contexto para carritos y routing",
+                    ImagenURL = "/images/proyectos/proyectoGamePlaceReact.png",
+                    Link ="https://github.com/nicocastx/Game-Place",
+                },
+                new ProyectDTO
+                {
+                    Titulo =  "Instrumenty",
+                    Descripcion = "Proyecto orientado al backend en donde desarrollo una aplicacion orientada al back-end",
+                    ImagenURL = "/images/proyectos/proyectoInstrumentyBackend.png",
+                    Link ="https://proyectofinalbackend-production-c2fa.up.railway.app/",
+                },
+                new ProyectDTO
+                {
+                    Titulo =  "Criptowin",
+                    Descripcion = "Criptowin es un proyecto realizado para demostrar mis conocimiento" +
+                    "adquiridos durante el cursado de Desarrollo Web en Coderhouse",
+                    ImagenURL = "/images/proyectos/proyectoCriptoWinDW.png",
+                    Link = "https://criptowin-4ctdw1k5j-nicocastx.vercel.app/"
+                },
+                new ProyectDTO
+                {
+                    Titulo =  "SteamSim",
+                    Descripcion = "SteamSim es mi proyecto desarrollado para el curso de Javascript en la Academia de Coderhouse ",
+                    ImagenURL = "/images/proyectos/proyectoSteamSimJS.png",
+                    Link ="https://steam-sim-coder-a0cy0c1h6-nicocastx.vercel.app/",
+                },
+            };
+        }
+    }
+}
